285690cd902bb07210f51be4fcf3ffb277735bf8
George Rodriguez <govintor@icloud.com>
2022-06-06 14:31:03 -0400

    second solution CW 016

--- js/solution016.js
+++ js/solution016.js
@@ -3,6 +3,10 @@ function gooseFilter (birds) {
   return birds.filter(b => !geese.includes(b));
 };
 
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter( bird => geese.indexOf(bird) < 0 );
+  };
 //Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
 
 //The geese are any strings in the following array, which is pre-populated in your solution:
@@ -11,5 +15,5 @@ function gooseFilter (birds) {
 //For example, if this array were passed as an argument:
 
 //["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
-//Your function would return the following array:
+//gitYour function would return the following array:
 
